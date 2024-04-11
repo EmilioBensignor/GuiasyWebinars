1394db98c0324d40baedf9d4822a42102f13b781
Emilio Bensignor <liobensignor@gmail.com>
2024-04-11 16:09:09 -0300

    Cambios Marcelo CMS

--- content/js/guias-y-webinarsT.js
+++ content/js/guias-y-webinarsT.js
@@ -42,6 +42,11 @@ $(document).ready(function () {
   $('.js-example-basic-single').select2();
 });
 
+function verGuia(pdf) {
+  $('#embedPDF').src= "/files/guias/" + pdf ;
+  $('modalGuia').show() ;   
+}
+
 document.addEventListener('DOMContentLoaded', function () {
   const modalGuia = document.getElementById('modalGuia');
   const abrirModalGuia = document.getElementById('abrirModalGuia');
